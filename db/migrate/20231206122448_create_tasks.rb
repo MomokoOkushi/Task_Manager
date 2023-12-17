c4d411224a336d901441985bf551dee018def112
EC2 Default User <ec2-user@ip-172-31-34-105.ap-northeast-1.compute.internal>
2023-12-17 04:54:20 +0000

    デプロイ修正

--- db/migrate/20231206122448_create_tasks.rb
+++ db/migrate/20231206122448_create_tasks.rb
@@ -5,7 +5,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
       t.integer :user_id, null: false #タスク管理者ID
       t.integer :group_id, null: false
       t.string :title, null: false
-      t.text :detail, null: false, default: ""
+      t.text :detail
       t.datetime :start_time, null: false
       t.timestamps
     end
