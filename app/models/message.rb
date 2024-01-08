3adfbd1cd5b2f780666facb8404c1fc037d0ecf7
EC2 Default User <ec2-user@ip-172-31-34-105.ap-northeast-1.compute.internal>
2024-01-08 12:43:26 +0000

    DM新着機能不具合修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,10 +8,10 @@ class Message < ApplicationRecord
   #送信者名の重複を防いだ状態で新着メッセージを取り出す。
   def self.received_latest_messages(current_user)
     send_user_ids = where(receive_user_id: current_user.id).pluck(:send_user_id).uniq #where直前のselfを省略済。
-    send_user_ids.map{|send_user_id|latest_message(send_user_id) }.sort_by{|message| message.created_at}.reverse
+    send_user_ids.map{|send_user_id|latest_message(send_user_id, current_user) }.sort_by{|message| message.created_at}.reverse
   end
 
-  def self.latest_message(send_user_id) #received_latest_messagesメソッドにて使用
-    where(send_user_id: send_user_id).last
+  def self.latest_message(send_user_id, current_user) #received_latest_messagesメソッドにて使用
+    where(send_user_id: send_user_id, receive_user_id: current_user.id).last
   end
 end
