ebbd08b9920a12d7b4bd91092dccb2d7309a2531
MomokoOkushi <146419260+MomokoOkushi@users.noreply.github.com>
2024-01-10 14:11:11 +0900

    復習

--- app/helpers/admin/tasks_helper.rb
+++ app/helpers/admin/tasks_helper.rb
@@ -1,12 +1,12 @@
 module Admin::TasksHelper
   # コメントの自然言語処理のスコアによる条件分岐(タスク一覧)
-  def task_has_negative_comments?(task) #感情分析スコアが-0.4以上０未満の時
+  def task_has_negative_comments?(task) #感情分析スコアが-0.4以上０未満の時。タスク詳細画面にて使用
     score = task_comments_score(task)
     return false unless score
     score < 0 && score >= -0.4
   end
 
-  def task_has_more_negative_comments?(task) #感情分析スコアが-0.4より低い時
+  def task_has_more_negative_comments?(task) #感情分析スコアが-0.4より低い時。タスク詳細画面にて使用
     score = task_comments_score(task)
     return false unless score
     score < -0.4
@@ -17,25 +17,25 @@ module Admin::TasksHelper
   end
 
   # タスク詳細の自然言語処理のスコアによる条件分岐
-  def task_has_negative_word?(task)
+  def task_has_negative_word?(task) #タスク本文にスコア0未満-0.4以上のものがあればtrueを返す。タスク一覧で使用。
     score = task.score
     return false unless score
     score < 0 && score >= -0.4
   end
 
-  def task_has_more_negative_word?(task)
+  def task_has_more_negative_word?(task) #タスク本文にスコア-0.4未満のものがあればtrueを返す。タスク一覧で使用。
     score = task.score
     return false unless score
     score < -0.4
   end
 
-  def comment_is_negative?(comment)  #感情分析スコアが-0.4以上０未満の時
+  def comment_is_negative?(comment)  #感情分析スコアが-0.4以上０未満の時。タスク詳細画面にて使用。
     score = comment.score
     return false unless score
     score < 0 && score >= -0.4
   end
 
-  def comment_is_more_negative?(comment) #感情分析スコアが-0.4より低い時
+  def comment_is_more_negative?(comment) #感情分析スコアが-0.4より低い時。タスク詳細画面にて使用。
     score = comment.score
     return false unless score
     score < -0.4  end
